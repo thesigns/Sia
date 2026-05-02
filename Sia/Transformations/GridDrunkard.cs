5e3bd1e050585bcbd8970cc06fb7882c9280b04c
Jakub <jwf.adamczyk@gmail.com>
2026-05-02 11:37:16 +0200

    Add `GridDrunkard` transformation for random walk-based grid value replacement

---
+++ Sia/Transformations/GridDrunkard.cs
@@ -0,0 +1,32 @@
+﻿namespace Sia.Transformations;
+
+public static class GridDrunkard
+{
+    public static Grid Drunkard(this Grid grid, byte oldValue, byte newValue, int steps,
+        Neighbourhood mode = Neighbourhood.Moore, Topology topology = Topology.Bounded)
+    {
+        var startPool = grid.IndicesOf(oldValue);
+        if (startPool.Count == 0) return grid;
+        
+        var poolIndex = grid.Rng.Next(startPool.Count);
+        var gridIndex = startPool[poolIndex];
+
+        grid.Front[gridIndex] = newValue;
+
+        while (steps-- > 0)
+        {
+            var valid = grid.GetNeighboursIndex(gridIndex, mode, topology)
+                .Where(n => grid.Front[n] == oldValue || grid.Front[n] == newValue)
+                .ToList();
+            
+            if (valid.Count == 0) break;
+            
+            var randomValid = valid[grid.Rng.Next(valid.Count)];
+
+            grid.Front[randomValid] = newValue;
+            gridIndex = randomValid;
+        }
+
+        return grid;
+    }
+}
